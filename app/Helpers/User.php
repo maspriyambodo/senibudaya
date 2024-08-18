1bda54496dd19e1fcb4efad443d46eedb191cdba
maspriyambodo <maspriyambodo@gmail.com>
2024-08-18 07:48:21 +0700

    update Helpers

--- app/Helpers/User.php
+++ app/Helpers/User.php
@@ -2,25 +2,83 @@
 
 namespace App\Helpers;
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use Illuminate\Support\Facades\Log;
+
 class User {
 
     public static function enkrip($text) {
-        $ciphering = "AES-128-CTR";
-        $iv_length = openssl_cipher_iv_length($ciphering);
-        $options = 0;
-        $encryption_iv = random_bytes($iv_length);
-        $encryption_key = openssl_digest(php_uname(), 'MD5', true);
-        $encryption = openssl_encrypt($text, $ciphering, $encryption_key, $options, $encryption_iv);
-        return $encryption;
+        $key = "BODO-13-GANTENG!";
+        $cipher = "AES-128-CBC";
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
+        $encryptedText = openssl_encrypt($text, $cipher, $key, 0, $iv);
+        $encryptedTextWithIv = base64_encode($iv . $encryptedText);
+        return $encryptedTextWithIv;
     }
 
     public static function dekrip($text) {
-        $ciphering = "AES-128-CTR";
-        $iv_length = openssl_cipher_iv_length($ciphering);
-        $decryption_iv = random_bytes($iv_length);
-        $options = 0;
-        $decryption_key = openssl_digest(php_uname(), 'MD5', true);
-        $decryption = openssl_decrypt($text, $ciphering, $decryption_key, $options, $decryption_iv);
-        return $decryption;
+        $key = "BODO-13-GANTENG!";
+        $cipher = "AES-128-CBC";
+        $decodedData = base64_decode($text);
+        $ivLength = openssl_cipher_iv_length($cipher);
+        $iv = substr($decodedData, 0, $ivLength);
+        $encryptedText = substr($decodedData, $ivLength);
+        $decryptedText = openssl_decrypt($encryptedText, $cipher, $key, 0, $iv);
+        return $decryptedText;
     }
+
+    public static function composeEmail($param) {
+        $data = [
+            'user_email' => $param['user_email'],
+            'pass_reset_link' => $param['pass_reset_link'],
+            'nama' => $param['nama']
+        ];
+        require base_path("vendor/autoload.php");
+        $mail = new PHPMailer(true);
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = 'smtp-relay.brevo.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = '5f0178001@smtp-brevo.com';
+        $mail->Password = '6zqyjUBQgwP8KnCV';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 587;
+        $mail->setFrom('humasbimasislam@kemenag.go.id', 'Repositori Seni Budaya Islam');
+        $mail->addAddress($param['user_email']);
+        $mail->addCC('maspriyambodo@gmail.com');
+//        $mail->addBCC($request->emailBcc);
+        $mail->addReplyTo('humasbimasislam@kemenag.go.id', 'Humas Bimas Islam');
+        $mail->isHTML(true);
+        $mail->Subject = $param['subject_title'];
+        $mail->Body = view($param['views_file'], $data)->render();
+        $mail->send();
+    }
+    
+//    public static function composeEmail($param) {
+//        $data = [
+//            'user_email' => $param['user_email'],
+//            'pass_reset_link' => $param['pass_reset_link'],
+//            'nama' => $param['nama']
+//        ];
+//        require base_path("vendor/autoload.php");
+//        $mail = new PHPMailer(true);
+//        $mail->SMTPDebug = 0;
+//        $mail->isSMTP();
+//        $mail->Host = 'sandbox.smtp.mailtrap.io';
+//        $mail->SMTPAuth = true;
+//        $mail->Username = '537948d7a33db0';
+//        $mail->Password = 'e226df579f836a';
+//        $mail->SMTPSecure = null;
+//        $mail->Port = 2525;
+//        $mail->setFrom('humasbimasislam@kemenag.go.id', 'Repositori Seni Budaya Islam');
+//        $mail->addAddress($param['user_email']);
+//        $mail->addCC('maspriyambodo@gmail.com');
+////        $mail->addBCC($request->emailBcc);
+//        $mail->addReplyTo('humasbimasislam@kemenag.go.id', 'Humas Bimas Islam');
+//        $mail->isHTML(true);
+//        $mail->Subject = $param['subject_title'];
+//        $mail->Body = view($param['views_file'], $data)->render();
+//        $mail->send();
+//    }
 }
