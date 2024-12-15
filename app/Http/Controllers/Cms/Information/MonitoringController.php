428f1f3dd840b07563e85d700828e2ec7c1ddd51
maspriyambodo <maspriyambodo@gmail.com>
2024-12-15 16:38:02 +0700

    add module monitoring

---
+++ app/Http/Controllers/Cms/Information/MonitoringController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Cms\Information;
+
+use App\Http\Controllers\Cms\AuthController;
+use App\Classes\ClassMenu;
+use App\Helpers\User as UserHelper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+use App\Models\TrMonitoring;
+use App\Models\TrMonitoringPetugas;
+use App\Models\DtaPegawai;
+use App\Models\MtProvinsi;
+use App\Models\MtKabupaten;
+use App\Models\TrMonitoringHasil;
+use App\Models\DtaLembagaSeni;
+use App\Models\DtaSeniman;
+use App\Models\DtaProgramSeni;
+use DataTables;
+use Image;
+
+class MonitoringController extends AuthController {
+
+    private $target = 'cms.information.monitoring';
+
+    public function index() {
+        $data = array_merge(
+                ClassMenu::view($this->target),
+                [
+                    'filter' => [],
+                ]
+        );
+        $column = array(
+            'id' => 'data',
+            'align' => array('center', 'left'),
+            'data' => array('button', 'no_monitoring', 'provinsi', 'kabupaten', 'created_at'),
+            'nosort' => array(0),
+        );
+        $data2 = array_merge($data, array('column' => $column));
+        return view($this->target, $data2);
+    }
+
+    public function json(Request $request) {
+        $exec = TrMonitoring::with('provinsi', 'kabupaten');
+
+        $this->applyFilters($exec, $request);
+
+        $berita = $exec->get();
+//        ddd($query);
+        return Datatables::of($berita)
+                        ->editColumn('created_at', fn($row) => \Carbon\Carbon::parse($row->created_at)->format('d/M/Y'))
+                        ->addColumn('button', fn($row) => $this->getActionButtons($row))
+                        ->rawColumns(['button'])
+                        ->make(true);
+    }
+
+    private function applyFilters($query, Request $request) {
+        if ($request->filled('keyword')) {
+            $query->where(function ($q) use ($request) {
+                $q->where('no_monitoring', $request->keyword);
+            });
+            $query->orWhereHas('provinsi', function ($q) use ($request) {
+                $q->where('nama', $request->keyword);
+            });
+            $query->orWhereHas('kabupaten', function ($q) use ($request) {
+                $q->where('nama', $request->keyword);
+            });
+        }
+    }
+
+    private function getActionButtons($row) {
+        if (!$this->edit && !$this->delete) {
+            return '';
+        }
+        $buttons = "<div class=\"btn-group dropright\">
+        <button class=\"btn btn-sm btn-icon btn-secondary dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\">
+            <i class=\"fas fa-ellipsis-v\"></i>
+        </button>
+        <div class=\"dropdown-menu dropright\">";
+
+        if ($this->edit) {
+            $buttons .= "<a id=\"edit\" class=\"dropdown-item has-icon\" href=\"" . url('/' . $this->page . '/form/' . $row->id) . "\">
+            <i class=\"fas fa-pencil-alt\"></i> Ubah Data</a>";
+        }
+
+        if ($this->delete) {
+            $buttons .= "<a id=\"del\" class=\"dropdown-item has-icon\" href=\"#\" data-toggle=\"modal\" data-target=\"#delete\">
+            <i class=\"fas fa-trash\"></i> Hapus Data</a>";
+        }
+
+        $buttons .= "</div></div>";
+
+        return $buttons;
+    }
+}
---
+++ resources/views/cms/information/monitoring.blade.php
@@ -0,0 +1,126 @@
+@include('cms.header')
+<div class="pcoded-main-container">
+    <div class="pcoded-content">
+        <div class="page-header">
+            <div class="page-block">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <div class="page-header-title">
+                            <h5 class="m-b-10">{{ $title }}</h5>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="page-header-title">
+                        </div>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('dashboard') }}"><i class="feather icon-home"></i></a></li>
+                            <li class="breadcrumb-item"><a href="#!">Informasi</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url($current) }}">{{ $title }}</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{ alertInfo() }}
+        <div class="bg-white p-2 m-4">
+            <div class="clearfix my-2"></div>
+            <div class="row g-3 align-items-center">
+                <div class="col-6">
+                    @if(isset($filter))
+                    {{ searchFilter($filter) }}
+                    @endif
+                </div>
+                <div class="col-6 text-right">
+                    @if($input)
+                    <a href="{{ url($current.'/add/') }}" class="btn btn-sm btn-warning"><i class="feather icon-plus"></i> Tambah Data</a>
+                    @else
+                    &nbsp;
+                    @endif
+                </div>
+            </div>
+            <div class="clear mt-4"></div>
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover" id="data">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>#</th>
+                            <th>Nomor Monitoring</th>
+                            <th>provinsi</th>
+                            <th>kabupaten</th>
+                            <th>Tanggal</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(function () {
+        var dTable = $("#data").DataTable({
+            processing: true,
+            serverSide: true,
+            paging: true,
+            ordering: true,
+            deferRender: true,
+            info: true,
+            ajax: {
+                url: "monitoring/json",
+                data: function (d) {
+                    d.keyword = $("#keyword").val();
+                }
+            },
+            order: [[0, "asc"]],
+            columnDefs: [
+                {className: "text-center text-nowrap", targets: [0]},
+                {className: "text-right text-nowrap", targets: []},
+                {orderable: false, targets: [1, 2, 3]}
+            ],
+            columns: [
+                {
+                    render: function (data, type, row, meta) {
+                        return meta.row + meta.settings._iDisplayStart + 1;
+                    }
+                },
+                {data: "button"},
+                {data: "no_monitoring"},
+                {
+                    render: function(data, type, row, meta){
+                            return row.provinsi.nama;
+                        }
+                },
+                {
+                    render: function(data, type, row, meta){
+                            return row.kabupaten.nama;
+                        }
+                },
+                {data: "created_at"},
+            ],
+            displayStart: 0,
+            pageLength: 10,
+            drawCallback: function () {
+                $('[data-toggle="popover"]').popover({container: "body"});
+                $("#input").off("click").on("click", function () {
+                    $.fn.start_length(dTable.page.info().start, dTable.page.info().length);
+                    $.fn.input();
+                });
+            },
+            initComplete: function () {
+                $.fn.start_length(0, 0, "", "");
+            },
+            dom: `<'row'<'col-sm-6 text-left'><'col-sm-6 text-right'B>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`
+        });
+
+        // Consolidate filter change events
+        const filterChangeHandler = delay(function () {
+            dTable.draw();
+        }, 50);
+
+        $("#keyword").keyup(delay(function (e) {
+            dTable.draw();
+            e.preventDefault();
+        }, 500));
+    });
+</script>
+@include('cms.footer')
