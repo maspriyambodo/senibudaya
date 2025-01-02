bb2cd11daf832571e7cf66703b935ec58c1a698d
maspriyambodo <maspriyambodo@gmail.com>
2025-01-02 21:44:14 +0700

    add module seniman

---
+++ app/Http/Controllers/Cms/Information/Seniman.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Cms\Information;
+
+use App\Http\Controllers\Cms\AuthController;
+use App\Classes\ClassMenu;
+use App\Helpers\User as UserHelper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Carbon;
+use App\Models\DtaSeniman;
+use App\Models\TrMonitoring;
+use App\Models\Provinsi;
+use App\Models\KabupatenKota;
+use DataTables;
+
+class Seniman extends AuthController {
+
+    private $target = 'seniman.index_seniman';
+
+    public function index() {
+        $data = array_merge(
+                ClassMenu::view($this->target),
+                [
+                    'filter' => []
+                ]
+        );
+        $column = array(
+            'id' => 'data',
+            'align' => array('center', 'left'),
+            'data' => array('button', 'nama', 'provinsi', 'kabupaten', 'alamat', 'bidang', 'karya', 'lembaga', 'created_at'),
+            'nosort' => array(0),
+        );
+        $data2 = array_merge($data, array('column' => $column));
+        return view($this->target, $data2);
+    }
+
+    public function json(Request $request) {
+        $exec = DtaSeniman::with('provinsi', 'kabupaten')->where('stat', 1);
+
+        $this->applyFilters($exec, $request);
+
+        $berita = $exec->orderBy('kabupaten')->get();
+        return Datatables::of($berita)
+                        ->editColumn('created_at', fn($row) => Carbon::parse($row->created_at)->translatedFormat('d/F/Y'))
+                        ->addColumn('button', fn($row) => $this->getActionButtons($row))
+                        ->rawColumns(['button'])
+                        ->make(true);
+    }
+
+    private function applyFilters($query, Request $request) {
+        if ($request->filled('keyword')) {
+            $query->where(function ($q) use ($request) {
+                $q->where('no_monitoring', $request->keyword)
+                        ->orWhere('tgl_monitoring', 'like', "%" . str_replace(['/', ' '], ['-', '-'], $request->keyword) . "%"); //format pencarian tanggal Y-m-d
+            });
+            $query->orWhereHas('provinsi', function ($q) use ($request) {
+                $q->where('nama', 'like', "%" . $request->keyword . "%");
+            });
+            $query->orWhereHas('kabupaten', function ($q) use ($request) {
+                $q->where('nama', 'like', "%" . $request->keyword . "%");
+            });
+        }
+    }
+
+    private function getActionButtons($row) {
+        if (!$this->edit && !$this->delete) {
+            return '';
+        }
+        $buttons = "<div class=\"btn-group dropright\">
+        <button class=\"btn btn-sm btn-icon btn-secondary dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\">
+            <i class=\"fas fa-ellipsis-v\"></i>
+        </button>
+        <div class=\"dropdown-menu dropright\">";
+
+        if ($this->edit) {
+            $buttons .= '<a id="view' . $row->id . '" class="dropdown-item has-icon" href="javascript:void(0);" onclick="vLembaga(' . $row->id . ');"><i class="fas fa-eye"></i> Lihat Data</a>';
+            $buttons .= '<a id="edit' . $row->id . '" class="dropdown-item has-icon" href="javascript:void(0);" onclick="eLembaga(' . $row->id . ');"><i class="fas fa-pencil-alt"></i> Ubah Data</a>';
+        }
+
+        if ($this->delete) {
+            $buttons .= '<a id="del' . $row->id . '" class="dropdown-item has-icon" href="javascript:void(0);" onclick="dLembaga(' . $row->id . ');"><i class="fas fa-trash"></i> Hapus Data</a>';
+        }
+
+        $buttons .= "</div></div>";
+
+        return $buttons;
+    }
+}
